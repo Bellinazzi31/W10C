eba22dd08fc1d41f39d38c2d7ab44aeb99f9a1c6
FelipeBellinazzi <bellinazzifelipe@gmail.com>
2020-12-01 17:16:30 -0700

    Added more Pokemons

--- js/pokemon.js
+++ js/pokemon.js
@@ -23,5 +23,40 @@ const pokemon = [
         attack: 20,
         image: 'https://cdn.bulbagarden.net/upload/b/b8/025Pikachu_LG.png'
     },
-    
+    {
+        name: 'Charizard',
+        hp: 180,
+        attack: 25,
+        image: 'https://vignette.wikia.nocookie.net/character-tribute/images/d/dd/Charizard-Transparent.png/revision/latest?cb=20190602204125'
+    },
+    {
+        name: 'Hitmochan',
+        hp: 130,
+        attack: 13,
+        image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/107.png'
+    },
+    {
+        name: 'Machamp',
+        hp: 140,
+        attack: 15,
+        image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/068.png'
+    },
+    {
+        name: 'Articuno',
+        hp: 175,
+        attack: 26,
+        image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/144.png'
+    },
+    {
+        name: 'Mew',
+        hp: 250,
+        attack: 30,
+        image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/151.png'
+    },
+    {
+        name: 'Mewtwo',
+        hp: 255,
+        attack: 30,
+        image: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/150.png'
+    },
 ]
